7547d4eb0738b3f59ce840816df444849fb1a6c8
cloud-sky-ops <ad5371032@gmail.com>
2025-03-16 21:42:09 +0000

    fix-kube-config-not-being-found-in-minikube

--- pkg/kubeclient/scanner.go
+++ pkg/kubeclient/scanner.go
@@ -3,15 +3,29 @@ package kubeclient
 import (
 	"context"
 	"fmt"
+	"log"
+	"os"
+	"path/filepath"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
 // ScanCluster connects to the Kubernetes API and scans for unused resources
 func ScanCluster(clusterName string) (string, error) {
-	config, err := rest.InClusterConfig()
+	// Fetch kubeconfig file path to get config values like "KUBERNETES_SERVICE_HOST", "KUBERNETES_SERVICE_PORT" and many more
+	// This logic counters the error ""unable to load in-cluster configuration, KUBERNETES_SERVICE_HOST and KUBERNETES_SERVICE_PORT must be defined""
+	// The default path for kube config file is "/home/username/.kube/config" and this code extracts the same and stores all values in config varibale
+
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		log.Fatalf("Error getting home directory: %v", err)
+	}
+
+	kubeconfigPath := filepath.Join(homeDir, ".kube", "config")
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfigPath)
+
 	if err != nil {
 		return "", fmt.Errorf("failed to create in-cluster config: %v", err)
 	}
