6dea6ce7e911ba94ee01cadb4f425c7a888a70f1
cloud-sky-ops <ad5371032@gmail.com>
2025-04-01 20:51:46 +0000

    update-GetConfigFile-utility-to-GetClientSet

--- internal/utils.go
+++ internal/utils.go
@@ -7,10 +7,11 @@ import (
 	"path/filepath"
 
 	"github.com/TwiN/go-color"
-	"k8s.io/client-go/rest"
+	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 )
-// Note: Only function names starting with capital letters are allowed to be 
+
+// Note: Only function names starting with capital letters are allowed to be
 // 		 exported outside their own package in golang
 
 // PrintError is a utility function to handle errors gracefully.
@@ -21,7 +22,7 @@ func PrintError(errorMessage string, err error) {
 	}
 }
 
-func GetKubeConfig() (*rest.Config, error) {
+func GetClientSet() (*kubernetes.Clientset, error) {
 	// Fetch kubeconfig file path to get config values like "KUBERNETES_SERVICE_HOST", "KUBERNETES_SERVICE_PORT" and many more
 	// This logic counters the error ""unable to load in-cluster configuration, KUBERNETES_SERVICE_HOST and KUBERNETES_SERVICE_PORT must be defined""
 	// The default path for kube config file is "/home/username/.kube/config" and this code extracts the same and stores all values in config varibale
@@ -38,6 +39,8 @@ func GetKubeConfig() (*rest.Config, error) {
 		return nil, fmt.Errorf("failed to create Kubernetes config: %v", err)
 	}
 
-	return config, nil
+	clientset, err := kubernetes.NewForConfig(config)
+
+	return clientset, nil
 
 }
